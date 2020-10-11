6aa3fb517b33304640470a2a487dfdefd588a1b9
Emiliano Fant <emilianofant@github.com>
2020-10-11 20:35:40 -0300

    chore: updated postcss configs

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ const purgecss = [
   '@fullhuman/postcss-purgecss',
   {
     // Specify the paths to all of the template files
-    content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+    content: ['./src/pages/**/*.{js,jsx,ts,tsx}', './src/components/**/*.{js,jsx,ts,tsx}'],
     // This is the function used to extract class names from the templates
     defaultExtractor: (content) => {
       // Capture as liberally as possible, including things like `h-(screen-1.5)`
@@ -16,9 +16,9 @@ const purgecss = [
 
 module.exports = {
   plugins: [
-    'postcss-import',
+    // 'postcss-import',
     'tailwindcss',
-    process.env.NODE_ENV === 'production' ? purgecss : undefined,
+    // purgecss,
     'postcss-preset-env',
   ],
 };
